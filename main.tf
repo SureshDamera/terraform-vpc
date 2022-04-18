df2506c1da91e2df0467e0df80aefed89db844cf
Suresh Damera <suresh.damera@innomate.com.au>
2022-04-18 23:47:26 +0530

    Adding parameterstore

--- main.tf
+++ main.tf
@@ -13,3 +13,10 @@ module "vpc" {
   single_nat_gateway  = true
   tags                = var.tags
 }
+
+resource "aws_ssm_parameter" "private-subnet-0" {
+  name        = "private-subnet-0"
+  description = "The password of onmostealth-aurora-${var.app_name}-instance-1 database"
+  value       = module.vpc.private_subnets[0]
+  tags        = var.tags
+}
